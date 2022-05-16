d68e4185ee03bc072e72a06d660ea44663c2cb8d
Mohammad Imran <mohammad.imran@diverselynx.com>
2022-05-16 15:08:06 +0530

    Swapping
    Signed-off-by: Mohammad Imran <mohammad.imran@diverselynx.com>

--- swap.js
+++ swap.js
@@ -1,6 +1,7 @@
-
 let x = 10, y = 5
-x = x ^ y
-y = x ^ y
-x = x ^ y
-console.log(x, y)
+x = x + y //15
+y = x - y //y = 15-5   Result y =10
+x = x - y // x= 15-10  Result x = 5
+console.log(x, y) 
+
+//Result 5, 10
