c937ca84ff2cc9947a697ec97f40b0daa981a0a4
imran7358 <shaikhmiran@gmail.com>
2022-08-26 18:51:58 +0530

    Bubble Sort
    Signed-off-by: imran7358 <shaikhmiran@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const flattenArray = (arr) => {
-    return arr.reduce((total, item)=>{
-      if(Array.isArray(item)){
-        total.push(...flattenArray(item))
+// const flattenArray = (arr) => {
+//     return arr.reduce((total, item)=>{
+//       if(Array.isArray(item)){
+//         total.push(...flattenArray(item))
+//       }
+//       else{
+//         total.push(item)
+//       }
+//       return total
+//     },[])
+//  };
+//  console.log(flattenArray([1, 2, 3, [4, 5, [6, 7]]]));
+
+const bubbleSort =(arr)=>{
+  for(let i = 0; i< arr.length; i++){
+    for(let j = i; j< arr.length; j++){
+      if(arr[j] > arr[j + 1]){
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp
       }
-      else{
-        total.push(item)
-      }
-      return total
-    },[])
- };
- console.log(flattenArray([1, 2, 3, [4, 5, [6, 7]]]));
+    }
+
+  }
+
+  return arr
+
+}
+
+console.log(bubbleSort([4,3,5,8,6]))
