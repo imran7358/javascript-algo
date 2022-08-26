30a5def4ab5e4a9865ddac7be5aea05c43234161
imran7358 <shaikhmiran@gmail.com>
2022-08-26 19:00:14 +0530

    Removing Dublicate Array
    Signed-off-by: imran7358 <shaikhmiran@gmail.com>

--- index.js
+++ index.js
@@ -11,20 +11,31 @@
 //  };
 //  console.log(flattenArray([1, 2, 3, [4, 5, [6, 7]]]));
 
-const bubbleSort =(arr)=>{
-  for(let i = 0; i< arr.length; i++){
-    for(let j = i; j< arr.length; j++){
-      if(arr[j] > arr[j + 1]){
-        var temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp
-      }
-    }
+// const bubbleSort =(arr)=>{
+//   for(let i = 0; i< arr.length; i++){
+//     for(let j = i; j< arr.length; j++){
+//       if(arr[j] > arr[j + 1]){
+//         var temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp
+//       }
+//     }
 
+//   }
+
+//   return arr
+
+// }
+
+// console.log(bubbleSort([4,3,5,8,6]))
+
+
+const arr = [1,1,2,3,2,4];
+const t =[]
+const sort = arr.map((n,i)=>{
+  if(t.indexOf(n) == -1){
+    t.push(n)
   }
+})
 
-  return arr
-
-}
-
-console.log(bubbleSort([4,3,5,8,6]))
+console.log(t)
