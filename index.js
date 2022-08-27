7252639e7690b7fce19c09ce19bde9b0b23bcdda
imran7358 <shaikhmiran@gmail.com>
2022-08-27 08:32:30 +0530

    Callback
    Signed-off-by: imran7358 <shaikhmiran@gmail.com>

--- index.js
+++ index.js
@@ -30,12 +30,24 @@
 // console.log(bubbleSort([4,3,5,8,6]))
 
 
-const arr = [1,1,2,3,2,4];
-const t =[]
-const sort = arr.map((n,i)=>{
-  if(t.indexOf(n) == -1){
-    t.push(n)
-  }
-})
+// const arr = [1,1,2,3,2,4];
+// const t =[]
+// const sort = arr.map((n,i)=>{
+//   if(t.indexOf(n) == -1){
+//     t.push(n)
+//   }
+// })
 
-console.log(t)
+// console.log(t)
+
+const greetings = (name, callback) =>{
+  console.log('Hello' + ' '+  name)
+  callback()
+}
+
+const callMe = ()=>{
+
+  console.log("This is Callback")
+}
+
+greetings('Kashif', callMe)
