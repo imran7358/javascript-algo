af7e63f1972498893ab6a9ccdc4f0631e2cb41bf
imran7358 <shaikhmiran@gmail.com>
2022-09-25 22:21:51 +0530

    Debouncing
    Signed-off-by: imran7358 <shaikhmiran@gmail.com>

---
+++ debouncing.js
@@ -0,0 +1,18 @@
+function getData(){
+
+    console.log("GetData")
+}
+
+function debounce(func, d){
+    let timer;
+    return function (...args){
+
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+            func()
+        }, d);
+    }
+
+}
+
+const myFunction = debounce(getData, 1000)
