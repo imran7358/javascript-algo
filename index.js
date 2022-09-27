7ee911ad6a548ca4030b8909b7aa99490c242acd
Mohammad Imran <mohammad.imran@diverselynx.com>
2022-09-27 20:03:53 +0530

    Add target Array
    Signed-off-by: Mohammad Imran <mohammad.imran@diverselynx.com>

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-function pangram (str) {
+function addTraget(arr, target){
 
-    mark = new Array(26).fill(false);
-    let index ;
+  for(var i = 0; i<arr.length; i++){
 
-    for(i = 0; i<= str.length; i++){
-        if('A' <= str[i] && str[i] <= 'Z'){
+    for( j = i + 1; j< arr.length; j++){
 
-            index = str.charCodeAt(i) - 'A'.charCodeAt(0)
-        }
+      if(arr[i] + arr[j] == target){
 
-        else if('a' <= str[i] && str[i] <= 'z')
-        {
-            index = str.charCodeAt(i) - 'a'.charCodeAt(0)
-
-        }
-
-        else continue;
-        mark[index] = true;
-    }
-
-    for( i = 0; i< 26; i++){
-        if(mark[i] == false)
-        return false;
-        
-        return   true;
+        return [i,j]
       }
+    }
+  }
 }
 
-
-let str = "The quick brown fox jumps over the lazy dog";
-
-pangram(str)
-if(pangram(str) == true ) {
-  
-  console.log('Pano')
-}
-
-else{
-  console.log('no pano')
-}
+console.log(addTraget([1,2,3,4,5],6))
