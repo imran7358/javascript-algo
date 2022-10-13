db282974de738c1a1e0b87ae6e6b43f71c9531e4
Mohammad Imran <mohammad.imran@diverselynx.com>
2022-10-13 17:10:47 +0530

    Closers functions
    Signed-off-by: Mohammad Imran <mohammad.imran@diverselynx.com>

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-var arr = 12345;
-var rev = 0;
-var lastdigit;
- 
-while(arr !== 0){
+// let myArray = [{
+//   'id': 123,
+//   'name': 'apples',
+//   'total': 30
+// },
+// {
+//   'id': 541,
+//   'name': 'oranges',
+//   'total': 42
+// },
+// {
+//   'id': 300,
+//   'name': 'bananas',
+//   'total': 18
+// }
+// ]
+// console.log(JSON.stringify(myArray.map(a => a.name)))
 
-  lastdigit = arr % 10;
-  rev = rev * 10 + lastdigit;
-  arr = Math.floor(arr / 10);
+function sum(a){
+  return function(b) {
+
+    return a+b
+  }
 }
-console.log(rev)
+console.log(sum(1)(3))
+
+var sum1 = function (x) {
+
+  return function (y){
+
+    if(y){
+
+      return sum1(x+y)
+    }
+    return  (x)
+  }
+}
+
+console.log(sum1(1)(2)(3)())
